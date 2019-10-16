e24a914cf03fa85b79841d59e5da3f7023ee7199
Rakib-RS <rakib.rscse@gmail.com>
2019-10-16 21:05:17 +0600

    read_a_task ,creat_a_task ,Upadate_a_task function created

--- api/routes/route.js
+++ api/routes/route.js
@@ -2,8 +2,8 @@
 module.exports = function(app){
     var todoList = require('../controllers/controller');
     app.route('/task')
-        .get(todoList.list_all_task)
-        .post(todoList.create_a_list);
+        .get(todoList.list_all_tasks)
+        .post(todoList.create_a_task);
     app.route('/task/:taskId')
         .get(todoList.read_a_task)
         .put(todoList.update_a_task)
