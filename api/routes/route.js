96ff611ab0b154699bb537b0e60f1f9b5c9548e9
Rakib-RS <rakib.rscse@gmail.com>
2019-10-16 16:51:21 +0600

    schema ,model and route created

---
+++ api/routes/route.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = function(app){
+    var todoList = require('../controllers/controller');
+    app.route('/task')
+        .get(todoList.list_all_pack)
+        .post(todoList.create_a_list);
+    app.route('/task/:taskId')
+        .get(todoList.read_a_task)
+        .put(todoList.update_a_task)
+        .delete(todoList.delete_a_task);
+};
