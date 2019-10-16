f13b956cf4a1db47e15d0dfcdc6634f8d4f2027a
Rakib-RS <rakib.rscse@gmail.com>
2019-10-16 22:06:50 +0600

    update something

--- api/models/model.js
+++ api/models/model.js
@@ -1,22 +1,24 @@
 'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var TaskSchema=new Schema({
-    name:{
-        type: String,
-        required: "Kindly enter the name of task"
-    },
-    Created_date: {
-        type:Date,
-        default:Date.now
-    },
-    statue: {
-        type:[{
-            type:String,
-            enum:['Pending','Ongoing','Completed']
-        }],
-        default:['Pending']
-    }
 
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
 });
-module.exports = mongoose.model('Tasks',TaskSchema);
+
+module.exports = mongoose.model('Tasks', TaskSchema);
