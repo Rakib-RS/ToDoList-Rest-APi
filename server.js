c1bf62810c9a9fc29b3c84cb490450651e1fc2f5
Rakib-RS <rakib.rscse@gmail.com>
2019-10-17 21:43:57 +0600

    mongodb cluster connected

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var port = process.env.PORT||3300;
 var mongoose = require('mongoose');
 var Task = require('./api/models/model');
 var bodyParser = require('body-parser');
-mongoose.connect('mongodb://localhost/Tododb',{useFindAndModify:true,useNewUrlParser:true,useUnifiedTopology:true});
+const uri="mongodb+srv://rakib:rakib@cluster0-0bgt6.mongodb.net/TodoDb?retryWrites=true&w=majority";
+//mongoose.connect('mongodb://localhost/Tododb',{useFindAndModify:true,useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(uri,{useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true});
 var db =mongoose.connection;
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -12,10 +14,11 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+var routes = require('./api/routes/route');
+routes(app);
 app.use((req,res)=>{
     res.status(404).send({url: req.originalUrl + "not found"});
 });
-var routes = require('./api/routes/route');
-routes(app);
 app.listen(port);
 console.log("App started from 3300 port "+port);
